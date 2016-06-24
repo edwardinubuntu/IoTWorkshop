61bbda026bf7285157391d914591c125d93a6e60
Edward Chiang <edwardubuntu@gmail.com>
2016-06-24 16:54:53 +0800

    Setup range of data

--- lasss/src/main/java/tw/soleil/lasss/MainActivity.java
+++ lasss/src/main/java/tw/soleil/lasss/MainActivity.java
@@ -8,6 +8,8 @@ import android.util.Log;
 import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
@@ -118,6 +120,31 @@ public class MainActivity extends AppCompatActivity {
         // set an alternative background color
         // mChart.setBackgroundColor(Color.GRAY);
 
+        LimitLine ll1 = new LimitLine(40f, "Super Hot");
+        ll1.setLineWidth(4f);
+        ll1.enableDashedLine(10f, 10f, 0f);
+        ll1.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_TOP);
+        ll1.setTextSize(10f);
+
+        LimitLine ll2 = new LimitLine(10f, "Cold");
+        ll2.setLineWidth(4f);
+        ll2.enableDashedLine(10f, 10f, 0f);
+        ll2.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
+        ll2.setTextSize(10f);
+
+        YAxis leftAxis = lineChart.getAxisLeft();
+        leftAxis.removeAllLimitLines(); // reset all limit lines to avoid overlapping lines
+        leftAxis.addLimitLine(ll1);
+        leftAxis.addLimitLine(ll2);
+        leftAxis.setAxisMaxValue(60);
+        leftAxis.setAxisMinValue(-20f);
+
+        leftAxis.enableGridDashedLine(10f, 10f, 0f);
+        leftAxis.setDrawZeroLine(false);
+
+        // limit lines are drawn behind data (and not on top)
+        leftAxis.setDrawLimitLinesBehindData(true);
+
         lineChart.getAxisRight().setEnabled(false);
 
         setLineChartData(tempData, humiData);
@@ -215,6 +242,7 @@ public class MainActivity extends AppCompatActivity {
 
             // set data
             lineChart.setData(data);
+
         }
 
         lineChart.invalidate();
